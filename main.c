06dc6891d04f8f995867e3e6970babf575e60e6c
Denis MOUREU-LARRANG <dmoureu-@e1r10p4.42.fr>
2017-03-18 00:49:40 +0100

    OK

--- main.c
+++ main.c
@@ -7,100 +7,86 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
 typedef enum { false, true } bool;
 
 #define TINY 1024
 #define SMALL 4096 //GETPAGESIZE
 
-
 typedef struct    s_chunk
 {
-  int          size;
-  void            *ptr;
-  struct s_chunk  *next;
+  int             content;
 }                 t_chunk;
 
-typedef struct s_block
+typedef struct    s_block
 {
-  int          size;
-  int          type;
-  t_chunk      *child;
+  int             total;
+  int             size;
   struct s_block  *next;
 }                 t_block;
 
-
-int chunk_count(t_chunk *chunk)
-{
-  t_chunk *current;
-  int     nb;
-
-  nb = 0;
-  current = chunk;
-  while (current)
-  {
-    nb++;
-    current = current->next;
-  }
-  return (nb);
-}
-
-
-
-t_block *getnewblock(int size, int type)
+t_block *getnewblock(int type)
 {
   t_block *block;
-
-  block = (t_block*)mmap(0, size , PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1,0);
+  int     i;
+  t_chunk *chunk;
+  i = 0;
+  block = (t_block*)mmap(0, type * 100 + (100*sizeof(t_chunk)) + sizeof(t_block) , PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1,0);
   if (block == MAP_FAILED)
   {
     perror("mmap");
     return (NULL);
   }
-  block->size = size;
-  block->type = type;
-  block->child = NULL;
+  block->total = type * 100 + (100*sizeof(t_chunk)) + sizeof(t_block);
+
+  printf("block TOTAL:%#x", block->total);
+  block->size = type;
   block->next = NULL;
+  while (i < 100)
+  {
+    chunk = block + sizeof(t_block) + (sizeof(t_chunk) * i);
+    printf("alloc:%p\n", chunk);
+    chunk->content = 0;
+    i++;
+  }
   return (block);
 }
 
-void    *blockstorechunk(t_block *block, int size)
+void    *blockstorechunk(t_block *block, int type, int size)
 {
+  int i;
   t_chunk *chunk;
-  t_chunk *lastchunk;
   void    *ptr;
-  int     nbchunk;
 
-  nbchunk = chunk_count(block->child);
-  chunk = (t_chunk*)(block + sizeof(t_block) + nbchunk * sizeof(t_chunk));
-  ptr =      (void*)(block + sizeof(t_block) + (100 * sizeof(t_chunk)) + nbchunk * size);
-  chunk->size = size;
-  chunk->ptr = ptr;
-
-  if (block->child == NULL)
-    block->child = chunk;
-  else
+  i = 0;
+  while (i < 100)
   {
-    lastchunk = block->child;
-    while (lastchunk->next)
-    {
-      lastchunk = lastchunk->next;
-    }
-    lastchunk->next = chunk;
+    chunk = (t_chunk*)(block + sizeof(t_block) + (sizeof(t_chunk) * i));
+    if (chunk->content == 0)
+      break;
+    i++;
   }
-
+  chunk->content = size;
+  ptr = (void*) (block + sizeof(t_block) + (100 * sizeof(t_chunk)) + i * type);
   return (ptr);
 }
 
-bool    blockcanstore(t_block *block, int size)
+bool    blockcanstore(t_block *block, int type)
 {
   int  sizefree;
-  if (block->type != size)
+  t_chunk *chunk;
+  int i;
+
+  i = 0;
+  if (block->size != type)
     return (false);
 
-  sizefree = block->size - sizeof(t_block) - ((sizeof(t_chunk) + block->type) * chunk_count(block->child));
-  if (sizefree > size + sizeof(t_chunk))
-    return (true);
+  while (i < 100)
+  {
+    chunk = (t_chunk*)(block + sizeof(t_block) + (sizeof(t_chunk) * i));
+    if (chunk->content == 0)
+      return (true);
+    i++;
+  }
   return (false);
 }
 
@@ -114,7 +100,7 @@ t_block *blocksmanager(t_block *first)
 }
 
 
-void *searchmapfor(int size)
+void *searchmapfor(int type, int size)
 {
   t_block *blocks;
   t_block *currentblock;
@@ -124,11 +110,11 @@ void *searchmapfor(int size)
   blockfound = false;
   //CREATE OR GET BLOCKS
   if(!(blocks = blocksmanager(NULL)))
-    blocks = blocksmanager(getnewblock(size * 100, size));
+    blocks = blocksmanager(getnewblock(type));
 
   //If can store in
-  if (blockcanstore(blocks, size))
-    return (blockstorechunk(blocks,size));
+  if (blockcanstore(blocks, type))
+    return (blockstorechunk(blocks,type,size));
 
   currentblock = blocks;
   while (currentblock->next && !blockfound)
@@ -137,12 +123,12 @@ void *searchmapfor(int size)
     blockfound = blockcanstore(currentblock, size);
   }
   if (blockfound)
-    return (blockstorechunk(currentblock,size));
+    return (blockstorechunk(currentblock,type,size));
   else
   {
-    currentblock->next = getnewblock(size * 100, size);
+    currentblock->next = getnewblock(type);
     if (blockcanstore(currentblock->next, size))
-      return (blockstorechunk(currentblock->next,size));
+      return (blockstorechunk(currentblock->next,type,size));
   }
 
   return (NULL);
@@ -160,7 +146,7 @@ int msize(int size)
 
 void *getfreemap(int size)
 {
-  return (searchmapfor(msize(size)));
+  return (searchmapfor(msize(size), size));
 }
 
 void *ft_malloc(int size)
@@ -168,23 +154,28 @@ void *ft_malloc(int size)
   return (getfreemap(size));
 }
 
-
 void show_alloc_mem()
 {
   t_block *blocks;
   t_block *current;
   t_chunk *chunk;
+  void *ptr;
+  int i;
 
+  i = 0;
   blocks = blocksmanager(NULL);
   current = blocks;
   while (current)
   {
-    printf("\nblock:%d addr:%p s:%d ===================%#x================\n", current->type, current, sizeof(t_block), current->size);
-    chunk = current->child;
-    while(chunk)
+    printf("\nblock:%d addr:%p s:%d ===================%#x======%p==========\n", current->total, current, sizeof(t_block), current->size, (current+current->total));
+
+    while (i < 100)
     {
-      printf("struct chunk  p:%p s:%d \n   Zone memoire:%p s:%d\n", chunk, sizeof(t_chunk), chunk->ptr, chunk->size);
-      chunk = chunk->next;
+      chunk = (t_chunk*)(current + sizeof(t_block) + (sizeof(t_chunk) * i));
+      ptr = (void*) (current + sizeof(t_block) + (100 * sizeof(t_chunk)) + (i * current->size));
+      if (chunk->content > 0)
+        printf("Chunk %d/%d %d %p\n", chunk->content, current->size, sizeof(t_chunk),ptr);
+      i++;
     }
     current = current->next;
   }
@@ -210,32 +201,96 @@ int main(void)
   char *test;
 
 
-  test = (char*)ft_malloc(2500);
+  test = (char*)ft_malloc(1020);
   printf(" [ %p ] ", test);
-  bzero(test, 10);
   strcpy(test,"HelloWorld    3 SMALL\n");
   printf("%s\n", test);
 
 
-  test = (char*)ft_malloc(2500);
+  test = (char*)ft_malloc(1020);
   printf(" [ %p ] ", test);
-  bzero(test, 10);
-  strcpy(test,"HelloWorld    5 SMALL\n");
+  strcpy(test,"HelloWorld    3 SMALL\n");
   printf("%s\n", test);
 
-
-  test = (char*)ft_malloc(2500);
+  test = (char*)ft_malloc(1020);
   printf(" [ %p ] ", test);
-  bzero(test, 10);
   strcpy(test,"HelloWorld    3 SMALL\n");
   printf("%s\n", test);
 
 
-  test = (char*)ft_malloc(2500);
+
+  test = (char*)ft_malloc(1020);
   printf(" [ %p ] ", test);
-  bzero(test, 10);
-  strcpy(test,"HelloWorld    5 SMALL\n");
+  strcpy(test,"HelloWorld    3 SMALL\n");
   printf("%s\n", test);
+
+  test = (char*)ft_malloc(1020);
+  printf(" [ %p ] ", test);
+  strcpy(test,"HelloWorld    3 SMALL\n");
+  printf("%s\n", test);
+
+  //
+  //
+  // test = (char*)ft_malloc(1020);
+  // printf(" [ %p ] ", test);
+  // strcpy(test,"HelloWorld    3 SMALL\n");
+  // printf("%s\n", test);
+  //
+  // test = (char*)ft_malloc(1020);
+  // printf(" [ %p ] ", test);
+  // strcpy(test,"HelloWorld    3 SMALL\n");
+  // printf("%s\n", test);
+  //
+  // show_alloc_mem();
+  //
+  // test = (char*)ft_malloc(100);
+  // printf(" [ %p ] ", test);
+  //
+  // strcpy(test,"HelloWorld    3 SMALL\n");
+  // printf("%s\n", test);
+
+  // test = (char*)ft_malloc(2500);
+  // printf(" [ %p ] ", test);
+  //
+  // strcpy(test,"HelloWorld    3 SMALL\n");
+  // printf("%s\n", test);
+  //
+  //
+  // test = (char*)ft_malloc(2500);
+  // printf(" [ %p ] ", test);
+  //
+  // strcpy(test,"HelloWorld    5 SMALL\n");
+  // printf("%s\n", test);
+  //
+  //
+  // test = (char*)ft_malloc(2500);
+  // printf(" [ %p ] ", test);
+  //
+  // strcpy(test,"HelloWorld    3 SMALL\n");
+  // printf("%s\n", test);
+  //
+  //
+  // test = (char*)ft_malloc(2500);
+  // printf(" [ %p ] ", test);
+  //
+  // strcpy(test,"HelloWorld    5 SMALL\n");
+  // printf("%s\n", test);
+  //
+  //
+  //   test = (char*)ft_malloc(5500);
+  //   printf(" [ %p ] ", test);
+  //
+  //   strcpy(test,"HelloWorld    5 SMALL\n");
+  //   printf("%s\n", test);
+  //
+  //   test = (char*)ft_malloc(55000);
+  //   printf(" [ %p ] ", test);
+  //
+  //   strcpy(test,"HelloWorld    5 SMALL\n");
+  //   printf("%s\n", test);
+  //
+
+    printf("[%p    %p]", test, test + sizeof(t_block) + 100 * sizeof(t_chunk));
 
   show_alloc_mem();
   //while(42);
